ac63d2d852ce0f94ed9b1bb5fcc1045dfef66309
sweeneyd <dan.sweeney90@gmail.com>
2025-07-13 23:15:17 -0400

    add repo example for interrupt-driven hardware timer/counter

---
+++ app_counter/Readme.md
@@ -0,0 +1,8 @@
+```
+west build -p always -b stm32f3_disco app_counter
+west flash
+```
+
+## Resource
+- [Hardware Timer](https://github.com/zephyrproject-rtos/zephyr/discussions/47033#discussioncomment-3065451)
+- [Zephyr HW Counter Example](https://github.com/zephyrproject-rtos/zephyr/tree/495c3b4d26f1b610ce63059430eab0b5a0842c40/samples/drivers/counter/alarm)
---
+++ app_counter/src/main.c
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2019 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/counter.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/gpio.h>
+
+#define LED0_NODE DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+#define DELAY 1000000
+#define ALARM_CHANNEL_ID 0
+
+struct counter_alarm_cfg alarm_cfg;
+
+static void test_counter_interrupt_fn(const struct device *counter_dev,
+				      uint8_t chan_id, uint32_t ticks,
+				      void *user_data)
+{
+	struct counter_alarm_cfg *config = user_data;
+	uint32_t now_ticks;
+	uint64_t now_usec;
+	int now_sec;
+	int err;
+
+	err = counter_get_value(counter_dev, &now_ticks);
+	if (!counter_is_counting_up(counter_dev)) {
+		now_ticks = counter_get_top_value(counter_dev) - now_ticks;
+	}
+
+	if (err) {
+		printk("Failed to read counter value (err %d)", err);
+		return;
+	}
+
+	now_usec = counter_ticks_to_us(counter_dev, now_ticks);
+	now_sec = (int)(now_usec / USEC_PER_SEC);
+
+	printk("!!! Alarm !!!\n");
+	printk("Now: %u\n", now_sec);
+
+	/* Set a new alarm with a double length duration */
+	// config->ticks = config->ticks * 2U;
+
+	printk("Set alarm in %u msec (%u ticks)\n",
+	       (uint32_t)(counter_ticks_to_us(counter_dev,
+					   config->ticks) / USEC_PER_SEC),
+	       config->ticks);
+
+	err = counter_set_channel_alarm(counter_dev, ALARM_CHANNEL_ID,
+					user_data);
+	if (err != 0) {
+		printk("Alarm could not be set\n");
+	}
+    gpio_pin_toggle_dt(&led);
+}
+
+int main(void)
+{
+    gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    printk("Configured LED0\n");
+
+	const struct device *const counter_dev = DEVICE_DT_GET(TIMER);
+	int err;
+
+	printk("Counter alarm sample\n\n");
+
+	if (!device_is_ready(counter_dev)) {
+		printk("device not ready.\n");
+		return 0;
+	}
+
+	counter_start(counter_dev);
+
+	alarm_cfg.flags = 0;
+	alarm_cfg.ticks = counter_us_to_ticks(counter_dev, DELAY);
+	alarm_cfg.callback = test_counter_interrupt_fn;
+	alarm_cfg.user_data = &alarm_cfg;
+
+	err = counter_set_channel_alarm(counter_dev, ALARM_CHANNEL_ID,
+					&alarm_cfg);
+	printk("Set alarm in %u sec (%u ticks)\n",
+	       (uint32_t)(counter_ticks_to_us(counter_dev,
+					   alarm_cfg.ticks) / USEC_PER_SEC),
+	       alarm_cfg.ticks);
+
+	if (-EINVAL == err) {
+		printk("Alarm settings invalid\n");
+	} else if (-ENOTSUP == err) {
+		printk("Alarm setting request not supported\n");
+	} else if (err != 0) {
+		printk("Error\n");
+	}
+
+	while (1) {
+		k_sleep(K_FOREVER);
+	}
+	return 0;
+}
